195517f752477920c8f1c31e2178b98d870935f9
dusong7 <dusong7@hotmail.com>
2016-06-19 18:31:37 +0800

    Update mp3player.java

--- mp3player.java
+++ mp3player.java
@@ -77,3 +77,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+///New Way
+package com.example.admin.testaudio;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mySound;
+    boolean isReleased = false;
+    boolean isPaused = false;
+    int nClickTimes = 0;
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        mySound.release();
+//        isReleased= true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mySound = MediaPlayer.create(this, R.raw.demo);
+        getSupportActionBar().hide();
+    }
+
+    public void playMusic(View view) {
+        ImageButton btn = (ImageButton)this.findViewById(R.id.button);
+
+//        String strCurBtn = btn.getText().toString();
+        Boolean isBtnClicked = btn.isPressed();
+        if (isBtnClicked)
+        {
+            nClickTimes++;
+        }
+
+        if (nClickTimes%2 == 1)
+        {
+            mySound.start();
+            btn.setImageDrawable(getResources().getDrawable(R.drawable.pause));;
+         }
+        else
+        {
+            mySound.pause();
+            btn.setImageDrawable(getResources().getDrawable(R.drawable.play));
+        }
+//        switch (isBtnClicked)
+//        {
+//            case false:
+//
+//                btn.setImageDrawable(getResources().getDrawable(R.drawable.play));
+//                mySound.pause();
+//                break;
+//            case "Play":
+//
+//                btn.setImageDrawable(getResources().getDrawable(R.drawable.pause));
+//                mySound.start();
+//                break;
+//            default:
+//                break;
+//        }
+
+
+    }
+}

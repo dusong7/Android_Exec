e3141082510a567016391039a1202dacf047f729
dusong7 <dusong7@hotmail.com>
2016-06-19 16:13:31 +0800

    Update mp3player.java

--- mp3player.java
+++ mp3player.java
@@ -1,5 +1,5 @@
 //fist right click res,  choose new ,-> android  resource directory , select resorce type raw, then copy mpe file to this fold.
-//
+//https://www.youtube.com/watch?v=V1ocJmXeQ28
 package com.example.admin.testaudio;
 
 import android.media.MediaPlayer;
@@ -9,6 +9,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mySound;
+    //在此处右击选择generate 选择override 选择onPause(), home键后声音不在播放
+    //在xml文件中onclick后点击灯泡形状可以自动添加onclick事件。
+   @Override
+    protected void onPause() {
+        super.onPause();
+        mySound.release();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

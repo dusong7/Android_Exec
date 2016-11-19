811c9a29ecc53fd18a17301d08fe2d9e943db8b6
dusong7 <dusong7@hotmail.com>
2016-11-19 20:57:13 +0800

    Update surfaceview.java

--- surfaceview.java
+++ surfaceview.java
@@ -5,7 +5,6 @@ package com.example.admin.surface_;
  */
 
 
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -18,11 +17,22 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.Console;
+import java.util.Random;
+
 public class surView extends SurfaceView  implements Callback{
 
     LoopThread thread;
     int nCirclePosX;
     int nCirclePosY;
+
+    int nReached = 0;
+    int nOriRecX = 600;
+    int nOriRecY = 500;
+    int nRecPosX;
+    int nRecPosY;
+    int nTabX[] = {2,6,2,2,9,2,7,3,8,2,7,7,5,8,5,3,10,6,4,8,3,10,3,9,3,7,9,2,6,7,5,9,4,8,10,8,2,};
+    int nTabY[] = {10,5,1,6,6,10,7,9,1,3,5,9,4,8,10,9,0,3,5,9,10,3,2,10,4,1,8,3,1,4,10,7,7,10,5,1};
     Canvas c = null;
 
     public surView(Context context, AttributeSet attrs) {
@@ -38,8 +48,11 @@ public class surView extends SurfaceView  implements Callback{
         holder.addCallback(this); //设置Surface生命周期回调
         thread = new LoopThread(holder, getContext());
 
+
         nCirclePosX = 100;
         nCirclePosY = 100;
+        nRecPosX = 600;
+        nRecPosY = 300;
     }
 
     @Override
@@ -111,7 +124,9 @@ public class surView extends SurfaceView  implements Callback{
 
                         //通过它来控制帧数执行一次绘制后休息50ms
                         Thread.sleep(50);
-                        nCirclePosY += 10;
+                        nCirclePosY = nCirclePosY + nReached/5 + 10;
+
+                        nRecPosX = nRecPosX - nReached/2 -  5;
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -126,18 +141,12 @@ public class surView extends SurfaceView  implements Callback{
         public void doDraw(Canvas c){
 
             //这个很重要，清屏操作，清楚掉上次绘制的残留图像
-            c.drawColor(Color.WHITE);
+            c.drawColor(Color.CYAN);
 
             DrawScore(c);
 
-            Paint paintT = new Paint();
-            paintT.setColor(Color.BLACK);
-            paintT.setTextSize(50);
-            String strPosY = "";
-            strPosY = nCirclePosY + "";
-            c.drawText(strPosY,100,200,paintT);
 
-            c.drawCircle(nCirclePosX,nCirclePosY,100, paint);
+            c.drawCircle(nCirclePosX,nCirclePosY,50, paint);
 
             if(radius > 100){
                 radius = 10f;
@@ -150,18 +159,44 @@ public class surView extends SurfaceView  implements Callback{
 
         public void DrawScore(Canvas c)
         {
-            c.drawColor(Color.WHITE);
-
-            c.translate(300, 100);
+            //c.drawColor(Color.WHITE);
 
             Paint paintT = new Paint();
             paintT.setColor(Color.BLACK);
             paintT.setTextSize(50);
             String strPosY = "";
-            strPosY = nCirclePosY + "";
-            c.drawText(strPosY,100,200,paintT);
+            strPosY ="当前Y距离 "  + nCirclePosY;
+            c.drawText(strPosY,300,50,paintT);
+            drawRectangle(c);
         }
 
+        public void drawRectangle(Canvas canvas)
+        {
+            Paint paint = new Paint();
+            paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(10);
+
+            canvas.drawRect(nRecPosX-50,nRecPosY-50,nRecPosX,nRecPosY,paint);
+
+            //canvas.drawRect(nRecPosX-50,350,nRecPosX,400,paint);
+            //canvas.drawArc(300,300,350,350,10,100,false,paint);
+            //System.out.println(nRecPosX);
+
+            if (nRecPosX < 0)
+            {
+                nReached += 1;
+
+                int nNumber;
+                Random random = new Random();
+                nNumber = random.nextInt(11);
+
+                nRecPosX = nOriRecX - 20 * nNumber;
+                nRecPosY = nOriRecY + 50 * nNumber;
+                System.out.println(nReached);
+                System.out.println(nRecPosX);
+                System.out.println(nRecPosY);
+            }
+        }
     }
 
 }
